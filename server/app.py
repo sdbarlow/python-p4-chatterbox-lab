b3956ca13764b6799757887af9aa204cd4d6fe20
sdbarlow <sethdbarlow@gmail.com>
2023-04-04 15:09:55 -0600

    all tests pass

--- server/app.py
+++ server/app.py
@@ -14,12 +14,79 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages_by_created_at = Message.query.order_by(Message.created_at).all()
+        messages_by_created_at_serialized = [
+            mg.to_dict() for mg in messages_by_created_at
+        ]
 
-@app.route('/messages/<int:id>')
+        response = make_response(
+            messages_by_created_at_serialized,
+            200
+        )
+
+        return response
+    
+    elif request.method == 'POST':
+        new_message = Message(
+            body = request.json.get("body"),
+            username = request.json.get("username"),
+        )
+
+        db.session.add(new_message)
+        db.session.commit()
+
+        new_message_dict = new_message.to_dict()
+
+        response = make_response(
+            new_message_dict,
+            201
+        )
+
+        return response
+    
+
+@app.route('/messages/<int:id>', methods=['PATCH', 'DELETE'])
 def messages_by_id(id):
+    if request.method == 'PATCH':
+
+        message = Message.query.filter(Message.id == id).first()
+
+        for attr in request.json:
+            setattr(message, attr, request.json.get(attr))
+
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict = message.to_dict()
+
+        response = make_response(
+            message_dict,
+            200
+        )
+
+        return response
+    
+    elif request.method == 'DELETE':
+        message = Message.query.filter(Message.id == id).first()
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Review deleted."
+        }
+
+        response = make_response(
+            response_body,
+            200
+        )
+
+        return response
+
+
     return ''
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -3,7 +3,11 @@ from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
 metadata = MetaData(naming_convention={
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
 })
 
 db = SQLAlchemy(metadata=metadata)
@@ -12,3 +16,12 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String)
+    body = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+
+    def __repr__(self):
+        return f'<Message {self.username}: {self.body}>'
+
